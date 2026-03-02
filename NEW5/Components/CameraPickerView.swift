ff03c9aa89f338f68cc372a44d6bab3a7d275c83
WarriorMind04 <88904471+WarriorMind04@users.noreply.github.com>
2026-03-02 16:44:03 +0100

    Code cleaning and new UI in the profile view

---
+++ NEW5/Components/CameraPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraPickerView.swift
+//  NEW5
+//
+//  Created by José Miguel Guerrero Jiménez on 02/03/26.
+//
+
+import SwiftUI
+
+struct CameraPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraPickerView()
+}
---
+++ NEW5/Models/AnalyzerType.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyzerType.swift
+//  NEW5
+//
+//  Created by José Miguel Guerrero Jiménez on 02/03/26.
+//
+
+import Foundation
